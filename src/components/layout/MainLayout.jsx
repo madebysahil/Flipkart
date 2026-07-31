41fd550ddabce639c66d3dd14a357781878c5ae0
ihaven309 <sm5807646@gmail.com>
2026-07-31 21:44:31 +0530

    Hide old global Header on Home page

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,12 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import BottomNavigation from './BottomNavigation';
 
 const MainLayout = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
   return (
     <div className="flex flex-col min-h-screen bg-background">
-      <Header />
+      {!isHomePage && <Header />}
       <main className="flex-grow pb-16 sm:pb-0">
         <Outlet />
       </main>
